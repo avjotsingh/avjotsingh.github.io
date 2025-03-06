3c2ac23043af228542454390025ef1ca4e53296f
avjotsingh <30839396+avjotsingh@users.noreply.github.com>
2025-03-06 20:56:50 +0000

    Deploying to gh-pages from @ avjotsingh/avjotsingh.github.io@8b35d9643ae27e4a9169a2dd8b00f4de68b7a79d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-news",
           title: "News",
           description: "",
